ab7af1e9d008eade53a2484afd1845f015a91d97
asimov <seyranmustafa4@gmail.com>
2025-04-17 20:18:17 +0300

    p

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
-// import { protectRoute } from "../middleware/auth.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,8 +8,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-// router.put("/update-profile", protectRoute, updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
-// router.get("/check", protectRoute, checkAuth);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
