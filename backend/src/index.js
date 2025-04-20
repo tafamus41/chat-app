9eb75fe35568ba45973c739facf18ebbbf1540a4
asimov <seyranmustafa4@gmail.com>
2025-04-21 01:46:52 +0300

    p

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
