9d496dc84d36c07db209aff325bd8e42077ff145
asimov <seyranmustafa4@gmail.com>
2025-04-21 01:53:35 +0300

    p

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -6,10 +5,9 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
 );
 
